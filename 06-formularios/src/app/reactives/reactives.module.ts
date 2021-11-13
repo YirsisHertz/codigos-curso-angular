4255328fdfa1cc585f6f2d0be2ec6dc38fa27261
YirsisHertz <yirsishertz02@gmail.com>
2021-11-13 01:22:42 +0100

    reactive forms

--- 06-formularios/src/app/reactives/reactives.module.ts
+++ 06-formularios/src/app/reactives/reactives.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-
+import { ReativeFormsComponent } from './reative-forms/reative-forms.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ReativeFormsComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [ReativeFormsComponent],
 })
-export class ReactivesModule { }
+export class ReactivesModule {}
---
+++ 06-formularios/src/app/reactives/reative-forms/reative-forms.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReativeFormsComponent } from './reative-forms.component';
+
+describe('ReativeFormsComponent', () => {
+  let component: ReativeFormsComponent;
+  let fixture: ComponentFixture<ReativeFormsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReativeFormsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReativeFormsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
