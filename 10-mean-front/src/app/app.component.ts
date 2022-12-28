3518cb727d9a9b4ef4c1966fa2d29e5333044f43
YirsisHertz <yirsishertz02@gmail.com>
2022-12-28 10:50:23 -0600

    no sw

--- 10-mean-front/src/app/app.component.ts
+++ 10-mean-front/src/app/app.component.ts
@@ -1,33 +1,12 @@
 import { Component } from '@angular/core';
-import { SwPush } from "@angular/service-worker";
-import { environment } from "../environments/environment"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mean-front';
 
-  constructor( readonly swPush: SwPush ){
-    this.startPushNotifications()
-  }
-
-  private async startPushNotifications() {
-
-    try {
-      
-      const sub = await this.swPush.requestSubscription({
-        serverPublicKey: environment.PUBLIC_VAPID_KEY_OF_SERVER,
-      });
-
-      console.log(sub)
-
-    } catch (err) {
-      console.error('Could not subscribe due to:', err);
-    }
-    
-  }
-
+  constructor() {}
 }
